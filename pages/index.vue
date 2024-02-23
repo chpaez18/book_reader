1ee9e60136ca434ad593505e6b08353b71174faa
christian <cpaez@setisl.com>
2024-02-23 16:24:14 +0100

    ultimos avances

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-   
+
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
+onMounted(() => {
+	const router = useRouter();
+	router.push('/auth/login');
+});
 </script>
