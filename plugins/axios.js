e9f42666624ff2e36833997b2ef77e174eccba4f
christian <cpaez@setisl.com>
2024-02-23 15:51:18 +0100

    ultimos avances

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((NuxtApp)=>{
 
     //axios.defaults.withCredentials = true;
     //axios.defaults.baseURL = 'http://localhost/api_book_reader/public/api'
-    axios.defaults.baseURL = 'http://3.86.2.156/api'
+    axios.defaults.baseURL = 'https://3.86.2.156/api'
     return {
         provide: {
             axios: axios
