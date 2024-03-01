8990698c4eaaaf5d15c77361e7a1d89c5974e040
christian <cpaez@setisl.com>
2024-03-01 17:55:29 +0100

    correcion en la pagina codes

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,8 +3,8 @@ import axios  from "axios"
 export default defineNuxtPlugin((NuxtApp)=>{
 
     //axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'http://localhost/api_book_reader/public/api'
-    //axios.defaults.baseURL = 'https://3.86.2.156/api'
+    //axios.defaults.baseURL = 'http://localhost/api_book_reader/public/api'
+    axios.defaults.baseURL = 'https://3.86.2.156/api'
     return {
         provide: {
             axios: axios
