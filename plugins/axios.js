0bdf83a934d8eb04b990957b829990104c11a4e1
christian <cpaez@setisl.com>
2024-03-01 17:16:15 +0100

    refactorizacion del codigo

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,10 @@
 import axios  from "axios"
 
 export default defineNuxtPlugin((NuxtApp)=>{
-    
-    const config = useRuntimeConfig();
-    this.api_url = config.public.API_URL;
-    
+
     //axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = api_url
-    //axios.defaults.baseURL = 'https://3.86.2.156/api'
+    //axios.defaults.baseURL = 'http://localhost/api_book_reader/public/api'
+    axios.defaults.baseURL = 'https://3.86.2.156/api'
     return {
         provide: {
             axios: axios
