af00ff8a96811a34b40afc23e9440b9f1763e2a2
christian <cpaez@setisl.com>
2024-03-01 17:04:53 +0100

    refactorizacion del codigo

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
      <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
      <link href="https://fonts.googleapis.com/css2?family=Vibur&display=swap" rel="stylesheet">
 
-	   <meta name="google-signin-client_id" content="762020545207-jec88b818l9tnivtj38su8ekjg2q1svs.apps.googleusercontent.com">
+	   <meta name="google-signin-client_id" content="995661716637-coo8ljo8ncc39dr5srev1lib73mjh39h.apps.googleusercontent.com">
     <NuxtLayout>
        <NuxtPage />
        <Background />
