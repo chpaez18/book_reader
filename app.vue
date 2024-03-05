c10905ee880a1c2695752c205b167145a91acb95
christian <cpaez@setisl.com>
2024-03-05 08:19:27 +0100

    cambios menores

--- app.vue
+++ app.vue
@@ -2,8 +2,6 @@
    <div class="xs:max-w-none ">
      <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
      <link href="https://fonts.googleapis.com/css2?family=Vibur&display=swap" rel="stylesheet">
-
-	   <meta name="google-signin-client_id" content="995661716637-coo8ljo8ncc39dr5srev1lib73mjh39h.apps.googleusercontent.com">
     <NuxtLayout>
        <NuxtPage />
        <Background />
